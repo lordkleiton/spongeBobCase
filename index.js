0d6d7b484d843b88f0da3c2f3a56099453dea36b
lord <m_s_r@outlook.com>
2019-06-07 10:23:04 -0300

    fazendo o módulo funcionar

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-spongeBobCase = (a) => {
+exports.spongeBobCase = function(a) {
     let m = ''
 
     for (let i = 0; i < a.length; i++) m += (i % 2 === 0) ? a[i].toUpperCase() : a[i].toLowerCase()
